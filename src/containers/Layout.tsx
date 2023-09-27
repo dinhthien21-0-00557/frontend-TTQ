438cc009d87e6d3e1b722db591d7c53572caa5ce
VOTRANMINHQUANG <100178893+VOTRANMINHQUANG@users.noreply.github.com>
2023-09-27 23:12:16 +0700

    update giao diện

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -1,24 +1,48 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import "../assets/css/Style.css";
-import Bodyheader from "./../pages/Bodyheader";
 import Headers from "./../pages/Header";
-import Menu from "../pages/Menu";
-import Slider from "../pages/Slider";
-import Directory from "./../pages/Directory";
-import Footercard from "../pages/Footercard";
-import Detai from "../source/Directory-detai";
+
+import Login from "./../pages/Login";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Detai from "../pages/Detai";
+import Register from "../pages/Register";
+import Home from "./Home";
+import Bodyheader from "./../pages/Bodyheader";
+import Sanpham from "../admin/Sanpham";
+import Themsanpham from "./../admin/Themsanpham";
+import Thongtindathang from "../admin/Thongtindamua";
+import Admin from "../contaiadmin/Admin";
+import Mouse from "./../pages/Mouse";
+import Bpco from "../pages/Bpco";
+import Bpgiaco from "./../pages/Bpgiaco";
+import Loginadmin from "./../pages/Loginadmin";
+import Registeradmin from "./../pages/Registeradmin";
+import Shoppingcart from "./../pages/Shoppingcart";
+import Sanphamdamua from "./../pages/Sanphamdamua";
+import EditSanpham from "../admin/Editsanpham";
 
 const Layout = () => {
   return (
     <>
-      <Headers />
-      <Bodyheader />
-      <Menu />
-      <Slider />
-      <Directory />
-      <Footercard />
-      <Detai />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="loginuser" element={<Login />} />
+        <Route path="registeruser" element={<Register />} />
+        <Route path="loginadmin" element={<Loginadmin />} />
+        <Route path="registeradmin" element={<Registeradmin />} />
+        <Route path="detai/:id" element={<Detai />} />
+        <Route path="sanpham" element={<Sanpham />} />
+        <Route path="themsanpham" element={<Themsanpham />} />
+        <Route path="thongtindathang" element={<Thongtindathang />} />
+        <Route path="admin" element={<Loginadmin />} />
+        <Route path="mouse" element={<Mouse />} />
+        <Route path="keyco" element={<Bpco />} />
+        <Route path="keygiaco" element={<Bpgiaco />} />
+        <Route path="shoppingcart" element={<Shoppingcart />} />
+        <Route path="shopping" element={<Sanphamdamua />} />
+        <Route path="/editsanpham/:id" element={<EditSanpham />} />
+      </Routes>
     </>
   );
 };
