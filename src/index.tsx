659891ddbd35b40a45f934e817e9e2b6b70bd79e
VOTRANMINHQUANG <100178893+VOTRANMINHQUANG@users.noreply.github.com>
2023-09-28 21:14:26 +0700

    lỗi import đã sửa xong

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
@@ -10,8 +10,6 @@ import "./assets/css/Style.css";
 import "./assets/img/LOGO1.png";
 import Layout from "./containers/Layout";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; // Import Provider
-import store from "./redux/store"; // Import store
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -19,11 +17,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      {" "}
-      {/* Wrap your app with Provider */}
-      <Layout />
-    </Provider>
+    {" "}
+    <Layout />
   </BrowserRouter>
 );
 
