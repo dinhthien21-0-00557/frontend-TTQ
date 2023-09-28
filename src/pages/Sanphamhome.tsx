06b053477e8226b04a197f3260121cc15edb1585
vu dinh thien 21-0-00557 <vudinhthien697@gmail.com>
2023-09-28 22:46:29 +0700

    chỉnh sửa giao diện

---
+++ src/pages/Sanphamhome.tsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
+
+import { firestore } from "../data/firebase";
+import Header from "./Header";
+import Bodyheader from "./Bodyheader";
+import Menu from "./Menu";
+import Footercard from "./Footercard";
+import Slider from "./Slider";
+
+interface Product {
+  TEN_SP: string;
+  LOAI: string;
+  GIA: string;
+  IMAGES: string;
+  id: string;
+}
+
+const Sanphamhome = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const snapshot = await firestore
+          .collection("PRODUCT")
+          .where("LOAI", "==", "Bàn Phím giả cơ")
+          .get();
+        const productsData: Product[] = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            TEN_SP: data.TEN_SP,
+            LOAI: data.LOAI,
+            GIA: data.GIA,
+            IMAGES: data.IMAGES,
+          };
+        });
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleClick = (id: string) => {
+    navigate(`/detai/${id}`);
+  };
+  return (
+    <>
+      <Header />
+      <Bodyheader />
+      <Menu />
+      <Slider />
+      <section className="directory-color">
+        <div className="container">
+          <div className="headdline">
+            <h3>Sản Phẩm Mới ra mắt</h3>
+          </div>
+          <div className="product-form row j-b-c-t">
+            {products.map((product, index) => (
+              <div className="l-4 product-items col" key={index}>
+                <div className="img-product-items">
+                  <img src={product.IMAGES} alt={product.TEN_SP} />
+                  <div className="product-items-body">
+                    <div className="">
+                      <span className="title-product-items">
+                        {product.LOAI}
+                      </span>
+                      <span className="comments-product-items">
+                        {product.TEN_SP}
+                      </span>
+                      <span className="price-product-items d-l-b">
+                        {product.GIA}
+                      </span>
+                      <div>
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          onClick={() => handleClick(product.id)}
+                        >
+                          <i className="bi-plus-lg" /> Xem Ngay
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <Footercard />
+    </>
+  );
+};
+
+export default Sanphamhome;
